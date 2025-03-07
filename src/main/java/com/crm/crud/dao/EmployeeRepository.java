b0e943530fe968299a6c1ebc9ff73f5fe8e15298
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-07 23:48:12 +0200

    added security configs

--- src/main/java/com/crm/crud/dao/EmployeeRepository.java
+++ src/main/java/com/crm/crud/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.crm.crud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.crm.crud.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
        
 }
