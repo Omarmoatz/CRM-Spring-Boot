967fb67f917adab3d66486c2cbcfe46751ca4a08
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-09 11:45:14 +0200

    add user , Authority models

---
+++ src/main/java/com/crm/crud/entity/Authority.java
@@ -0,0 +1,25 @@
+package com.crm.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "authority", nullable = false)
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "username", nullable = false)
+    private User user;  // Many Authorities belong to one User
+}
--- src/main/java/com/crm/crud/entity/User.java
+++ src/main/java/com/crm/crud/entity/User.java
@@ -1,5 +1,29 @@
 package com.crm.crud.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private Set<Authority> authorities;  // One User can have many Authorities
     
 }
